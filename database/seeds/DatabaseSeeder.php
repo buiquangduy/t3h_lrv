395485c416b95d76a492cbde6a63114d19b5f95b
thienth6557 <thienth6557@massgenie.com>
2016-09-28 17:18:24 +0700

    create dataseed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        DB::table('users')->delete();
+
+        $users = [
+            ['name' => 'admin', 'email' => 'admin@t-master.com', 'password'=> Hash::make('admin123')],
+            ['name' => 'Ryan giggs', 'email' => 'someone1@t-master.com', 'password'=> Hash::make('admin123')],
+            ['name' => 'Ruby on rails', 'email' => 'someone2@t-master.com', 'password'=> Hash::make('admin123')],
+            ['name' => 'python', 'email' => 'someone3@t-master.com', 'password'=> Hash::make('admin123')]
+        ];
+
+        foreach($users as $user){
+            User::create($user);
+        }
+        Model::reguard();
     }
 }
